b47f7df9a3fcf84afcef49e8d891f6c257aa7637
Enda Kelly <kelly.enda@gmail.com>
2021-05-29 16:56:52 +0100

    added: endpoint logic for strategies

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from db import config
 import sqlite3
 from datetime import date
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -58,3 +59,42 @@ def index(request: Request, symbol):
     prices = cursor.fetchall()
 
     return templates.TemplateResponse("stock_detail.html", {"request": request, "stock": row, "bars": prices, "strategies": strategies})
+
+
+@app.post("/apply_strategy")
+def apply_strategy(strategy_id: int = Form(...), stock_id: int = Form(...)):
+    connection = sqlite3.connect(config.DB_FILE)
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        INSERT INTO stock_strategy (stock_id, strategy_id) VALUES (?, ?)
+    """, (stock_id, strategy_id))
+
+    connection.commit()
+
+    return RedirectResponse(url=f"/strategy/{strategy_id}", status_code=303)
+
+@app.get("/strategy/{strategy_id}")
+def strategy(request: Request,strategy_id):
+    connection = sqlite3.connect(config.DB_FILE)
+    connection.row_factory = sqlite3.Row
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT id, name
+        FROM strategy
+        WHERE id = ?
+    """, (strategy_id,))
+
+    strategy = cursor.fetchone()
+
+    cursor.execute("""
+        SELECT symbol, name
+        FROM stock JOIN stock_strategy on stock_strategy.stock_id = stock.id
+        WHERE strategy_id = ?
+    """, (strategy_id,))
+
+    stocks = cursor.fetchall()
+
+    return templates.TemplateResponse("strategy.html", {"request": request, "stocks": stocks, "strategy": strategy})
